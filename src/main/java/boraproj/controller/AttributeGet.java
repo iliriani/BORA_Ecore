56ce92d64c6a46439fe73aa01d2f159b8c9fdcd1
Ilirian Ibrahimi <32292023+iliriani@users.noreply.github.com>
2023-12-09 09:52:35 +0100

    Update AttributeGet.java - with class recommendations based on attributes

--- src/main/java/boraproj/controller/AttributeGet.java
+++ src/main/java/boraproj/controller/AttributeGet.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import boraproj.services.AttributeForRecommendations;
 import boraproj.services.AttributeService;
 import boraproj.services.RepoConnect;
 
@@ -19,6 +20,8 @@ public class AttributeGet {
 	RepoConnect stored;
 	@Autowired
 	AttributeService attr_service;
+	@Autowired
+	AttributeForRecommendations attr_class_recommendation;
 	
 	ArrayList<String> retried_ngrams;
 	ArrayList<String> model_ngram;
@@ -32,6 +35,7 @@ public class AttributeGet {
 	public AttributeGet(String repository) {
 		stored = new RepoConnect(repository);
 		attr_service = new AttributeService(repository);
+		attr_class_recommendation = new AttributeForRecommendations();
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value="attributes")
@@ -72,6 +76,12 @@ public class AttributeGet {
 	public LinkedHashMap getDomainAttributesWithDataTypes(@RequestParam String cl, @RequestParam String domain) {
 		
 		return attr_service.getAttributesWithDataTypesDomainRelevant(cl, domain);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="classRecommendationsFromAttributes")
+	public ArrayList getClassRecommendationsFromAttrSimilarities(@RequestParam ArrayList<String> class_attrs) {
+		
+		return attr_class_recommendation.getClassesWithSameAttributes(class_attrs);
 	}
 	
 }
